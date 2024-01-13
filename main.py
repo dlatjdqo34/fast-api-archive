0972004b409d62225f4128c32d882c9215f806aa
dlatjdqo34 <isb9000@naver.com>
2024-01-13 18:32:59 +0000

    feat: add css for main page

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return FileResponse('html/main.html')
+    return FileResponse("html/main.html")
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/{css_path}/{css_name}.css")
+def read_css(css_path:str, css_name: str):
+    css = "css/" + css_path + "/" + css_name + ".css"
+    return FileResponse(css)
